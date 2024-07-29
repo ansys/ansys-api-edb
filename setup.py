76fb93e5e07d939ef68c32c4a2a8f7db0b99b75b
drewm102 <103221973+drewm102@users.noreply.github.com>
2024-07-29 17:17:24 -0600

    MAINT: Add support for protobuf 4.x (#25)
    (cherry picked from commit 618773e3366192dea7e018f85026a153bdfb6086)

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         url=f"https://github.com/ansys/{package_name}",
         license="MIT",
         python_requires=">=3.7",
-        install_requires=["grpcio>=1.44", "protobuf>=3.19.3,<4"],
+        install_requires=["grpcio>=1.44", "protobuf>=3.19.3,<5"],
         packages=setuptools.find_namespace_packages(".", include=("ansys.*",)),
         package_data={
             "": ["*.proto", "*.pyi", "py.typed", "VERSION"],
